b67eb43d6f5540e5622f6f7e104fdee5c6a31f16
kunal <kunaljha015@gmail.com>
2023-12-28 22:41:31 +0530

    react-router-dom updated to v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/layout/Navbar'
 import Alert from './components/layout/Alert'
@@ -11,26 +11,26 @@ import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
 
 const App = () => {
-	return (
-		<GithubState>
-			<AlertState>
-				<Router>
-					<div className='App'>
-						<Navbar />
-						<div className='container'>
-							<Alert />
-							<Switch>
-								<Route exact path='/' component={Home} />
-								<Route exact path='/about' component={About} />
-								<Route exact path='/users/:login' component={User} />
-								<Route component={NotFound} />
-							</Switch>
-						</div>
-					</div>
-				</Router>
-			</AlertState>
-		</GithubState>
-	)
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/users/:login' element={<User />} />
+                <Route element={<NotFound />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  )
 }
 
 export default App
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,106 +1,108 @@
 import React, { Fragment, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
 import GithubContext from '../../context/github/githubContext'
 
-const User = ({ match }) => {
-	const githubContext = useContext(GithubContext) // initialising githubContext in User component
-	const { user, loading, getUser, getUserRepos } = githubContext
+const User = () => {
+  const githubContext = useContext(GithubContext) // initialising githubContext in User component
+  const { user, loading, getUser, getUserRepos } = githubContext
 
-	useEffect(() => {
-		getUser(match.params.login)
-		getUserRepos(match.params.login)
-		// eslint-disable-next-line
-	}, [])
+  const params = useParams()
 
-	const {
-		name,
-		company,
-		avatar_url,
-		location,
-		bio,
-		blog,
-		login,
-		html_url,
-		followers,
-		following,
-		public_repos,
-		public_gists,
-		hireable,
-	} = user
+  useEffect(() => {
+    getUser(params.login)
+    getUserRepos(params.login)
+    // eslint-disable-next-line
+  }, [])
 
-	return loading ? (
-		<Spinner />
-	) : (
-		<Fragment>
-			<Link to='/' className='btn btn-light'>
-				Back To Search
-			</Link>
-			Hireable:{'     '}
-			{hireable ? (
-				<i className='fas fa-check text-success' />
-			) : (
-				<i className='fas fa-times-circle text-danger' />
-			)}
-			<div className='card grid-2'>
-				<div className='all-center'>
-					<img
-						src={avatar_url}
-						alt='avatar_url'
-						className='round-img'
-						style={{ width: '150px' }}
-					/>
-					<h1>{name}</h1>
-					<h4>Location: {location}</h4>
-				</div>
-				<div>
-					{bio && (
-						<Fragment>
-							<h2 style={{ textDecorationLine: 'underline' }}>Bio</h2>
-							<p>{bio}</p>
-							<a href={html_url} className='btn btn-dark my-1'>
-								Visit GitHub Profile
-							</a>
-						</Fragment>
-					)}
-					<ul>
-						<li>
-							{login && (
-								<Fragment>
-									<strong>Username: </strong>
-									{login}
-								</Fragment>
-							)}
-						</li>
-						<li>
-							{company && (
-								<Fragment>
-									<strong>Company: </strong>
-									{company}
-								</Fragment>
-							)}
-						</li>
-						<li>
-							{blog && (
-								<Fragment>
-									<strong>Website: </strong>
-									<a href={blog}>{blog}</a>
-								</Fragment>
-							)}
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div className='card text-center'>
-				<div className='badge badge-primary'>Followers: {followers}</div>
-				<div className='badge badge-success'>Following: {following}</div>
-				<div className='badge badge-light'>Public Repos: {public_repos}</div>
-				<div className='badge badge-dark'>Public Gists: {public_gists}</div>
-			</div>
-			<Repos />
-		</Fragment>
-	)
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/' className='btn btn-light'>
+        Back To Search
+      </Link>
+      Hireable:{'     '}
+      {hireable ? (
+        <i className='fas fa-check text-success' />
+      ) : (
+        <i className='fas fa-times-circle text-danger' />
+      )}
+      <div className='card grid-2'>
+        <div className='all-center'>
+          <img
+            src={avatar_url}
+            alt='avatar_url'
+            className='round-img'
+            style={{ width: '150px' }}
+          />
+          <h1>{name}</h1>
+          <h4>Location: {location}</h4>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h2 style={{ textDecorationLine: 'underline' }}>Bio</h2>
+              <p>{bio}</p>
+              <a href={html_url} className='btn btn-dark my-1'>
+                Visit GitHub Profile
+              </a>
+            </Fragment>
+          )}
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong>
+                  {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong>
+                  {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong>
+                  <a href={blog}>{blog}</a>
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-success'>Following: {following}</div>
+        <div className='badge badge-light'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      </div>
+      <Repos />
+    </Fragment>
+  )
 }
 
 export default User
